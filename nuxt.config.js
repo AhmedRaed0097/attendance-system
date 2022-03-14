3db453a543b7e7bae6589f0639eac9e26453f247
AhmedRaed0097 <ahmedraed0097@gmail.com>
2022-03-14 16:10:51 +0300

    test commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
 
   ssr: false,
-
+// test
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
