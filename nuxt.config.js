46db10fa56c4764bcffb1a00ba283f0322cdd490
AhmedRaed0097 <83637337+AhmedRaed0097@users.noreply.github.com>
2022-03-14 15:52:13 +0300

    change mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,10 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
 
-  // ssr:false,
-  // target: 'server',
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - attendance-pwa-app',
