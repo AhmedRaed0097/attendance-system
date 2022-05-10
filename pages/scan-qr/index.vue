e75332e5edab872d9d3cfbfbe2336bec0c8e2b50
A7medRaed <83637337+AhmedRaed0097@users.noreply.github.com>
2022-05-10 14:09:07 +0300

    change student id

--- pages/scan-qr/index.vue
+++ pages/scan-qr/index.vue
@@ -67,7 +67,7 @@ export default {
       alert('start')
 
       let payload = JSON.parse(result)
-      payload.student_id = 1
+      payload.student_id = 3
       this.$store.dispatch('students/scanQr', payload).then((response) => {
         console.log('response ', response)
       })
@@ -80,7 +80,7 @@ export default {
     },
     onImageDecode(result) {
       let payload = JSON.parse(result)
-      payload.student_id = 3
+      payload.student_id = 1
       this.$store.dispatch('students/scanQr', payload).then((response) => {
         console.log('response ', response)
       })
