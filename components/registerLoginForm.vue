70a795012bbc1fb6454fe7db6da0f38ec2c0c7e2
A7medRaed <83637337+AhmedRaed0097@users.noreply.github.com>
2022-06-05 21:27:59 +0300

    some updates

--- components/registerLoginForm.vue
+++ components/registerLoginForm.vue
@@ -1,68 +1,65 @@
 <template>
   <v-card width="600" height="550" class="tw-my-9">
-      <v-card-title class="flex tw-justify-center">
-          <h2 class="tw-text-2xl">
-              تسجيل الدخول
-          </h2>
-      </v-card-title>
-      <v-form ref="form" v-model="valid" lazy-validation>
-   <v-container class="!tw-items-start !tw-pt-14">
+    <v-card-title class="flex tw-justify-center">
+      <h2 class="tw-text-2xl">تسجيل الدخول</h2>
+    </v-card-title>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-container class="!tw-items-start !tw-pt-14">
         <v-row>
-            <v-col cols="12">
-                <v-text-field
-                label="البريد الإلكتروني"
-                outlined
-                 />
+          <v-col cols="12">
+            <v-text-field label="البريد الإلكتروني" outlined />
+          </v-col>
+          <v-col cols="6">
+            <v-text-field label="كلمة المرور" outlined />
+          </v-col>
+          <v-col cols="6">
+            <v-text-field label="تأكيد كلمة المرور" outlined />
+          </v-col>
 
-            </v-col>
-            <v-col cols="6">
-                <v-text-field
-                label="كلمة المرور"
-                outlined
-                 />
-
-            </v-col>
-            <v-col cols="6">
-                <v-text-field
-                label="تأكيد كلمة المرور"
-                outlined
-                 />
-
-            </v-col>
-            
-            <v-col cols="6" class="tw-mx-auto tw-bg-green-400 ">
-                <v-radio-group class="tw-bg-red-400 !w-full tw-flex tw-flex-row tw-gap-4 tw-justify-between" v-model="selectUserType">
-                    <v-radio v-model="selectUserType" value="student" label="طالب"></v-radio>
-                    <v-radio v-model="selectUserType" value="lecturer" label="محاضر"></v-radio>
-                </v-radio-group>
-
-            </v-col>
-            <v-col cols="12">
-                <div class="tw-flex tw-justify-center">
-
-                <v-btn  outlined class="!tw-p-5">
-                    دخول
-                </v-btn>
-                </div>
-            </v-col>
+          <v-col cols="6" class="tw-mx-auto">
+            <v-radio-group v-model="selectUserType">
+              <div clas="tw-flex tw-bg-red-500">
+                <v-radio
+                  v-for="(type,i) in userTypes"
+                  :key="i"
+                  :label="type.label"
+                  :value="type.value"
+                  color="#000"
+                  class="tw-mb-0"
+                ></v-radio>
+              </div>
+            </v-radio-group>
+          </v-col>
+          <v-col cols="12">
+            <div class="tw-flex tw-justify-center">
+              <v-btn outlined class="!tw-p-5"> دخول </v-btn>
+            </div>
+          </v-col>
         </v-row>
-        </v-container>
-      </v-form>
+      </v-container>
+    </v-form>
   </v-card>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            userType:['student','lecturer'],
-            selectUserType:'student',
-            valid : true
-        }
+  data() {
+    return {
+      userTypes: [
+        {
+          label: 'طالب',
+          value: 'student',
+        },
+        {
+          label: 'محاضر',
+          value: 'lecture',
+        },
+      ],
+      selectUserType: 'student',
+      valid: true,
     }
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
