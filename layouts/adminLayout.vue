6178f7c5f6da57f276c629410781aa61ee6fd51d
A7medRaed <83637337+AhmedRaed0097@users.noreply.github.com>
2022-06-22 21:13:49 +0300

    add app bar

--- layouts/adminLayout.vue
+++ layouts/adminLayout.vue
@@ -18,11 +18,37 @@
       app
       class="app-bar !tw-bg-primary"
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+          <img
+            width="30"
+            height="30"
+            src="../assets/images/admin/burger.svg"
+            alt="burger"
+          />
+      </v-app-bar-nav-icon>
 
       <nav-drawer @drawerClosed="onDrawerClosed" :drawer="drawer" />
+      <span class="!tw-text-base sm:!tw-text-xl tw-text-first">
+        {{ app_bar_text }}
+      </span>
+      <div class="tw-flex tw-gap-x-2.5 !tw-ml-[-10px]">
+        <img
+          width="25"
+          v-if="$auth.loggedIn"
+          @click="$auth.logout()"
+          class="back-icon tw-mr-6 tw-cursor-pointer"
+          src="../assets/images/auth/logout.svg"
+          alt="logout"
+        />
+        <img
+          width="25"
+          v-if="$route.name !== 'index'"
+          @click="$router.back()"
+          src="../assets/images/home/back-icon.svg"
+          alt=""
+        />
+      </div>
     </v-app-bar>
-
     <v-row>
       <v-col cols="12">
         <v-main>
@@ -42,12 +68,24 @@ export default {
   // middleware: 'checkUserType',
   data: () => ({
     drawer: null,
+    app_bar_text:''
   }),
 
   watch: {
     '$route.name': {
       handler() {
         this.drawer = !this.drawer
+        if (this.$route.name === 'index') {
+          this.app_bar_text = 'الرئيسية'
+        } else if (this.$route.name === 'lectures-table') {
+          this.app_bar_text = 'جدول المحاضرات'
+        } else if (this.$route.name === 'generate-qr') {
+          this.app_bar_text = 'إنشاء QR CODE'
+        } else if (this.$route.name === 'attendance-table') {
+          this.app_bar_text = 'جدول الحضور'
+        } else if (this.$route.name === 'manual-attendance') {
+          this.app_bar_text = 'التحضير اليدوي'
+        }
       },
     },
   },
