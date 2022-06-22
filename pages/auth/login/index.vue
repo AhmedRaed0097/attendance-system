c4b5298899aa2a365250651526d0fa7dff13788c
A7medRaed <83637337+AhmedRaed0097@users.noreply.github.com>
2022-06-22 18:08:49 +0300

    make console log to check function

--- pages/auth/login/index.vue
+++ pages/auth/login/index.vue
@@ -10,7 +10,9 @@ created() {
   window.addEventListener('beforeunload', this.handler)
 },
 methods: {
+  // try to solve production error duplicate name space 'auth'
   handler: function handler() {
+    console.log('reload');
      this.$router.push('/')
 
   }
