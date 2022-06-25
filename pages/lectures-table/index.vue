540b368ba3de02b716dd389a30cf94bd49472017
A7medRaed <ahmedraed0097@gmail.com>
2022-06-26 02:28:14 +0300

    fix store file name

--- pages/lectures-table/index.vue
+++ pages/lectures-table/index.vue
@@ -8,15 +8,24 @@
         :alert-data="alertData"
         @closeModal="isAlertClosed"
       />
-      <v-col v-if="$vuetify.breakpoint.mdAndUp" cols="12" class="tw-mt-12 tw-mb-8">
+      <v-col
+        v-if="$vuetify.breakpoint.mdAndUp"
+        cols="12"
+        class="tw-mt-12 tw-mb-8"
+      >
         <h3 class="!tw-text-center !tw-text-4xl">جدول المحاضرات</h3>
       </v-col>
-      <v-col cols="12" class="!tw-px-6">
+      <v-col v-if="lecturesData.length > 0" cols="12" class="!tw-px-6">
         <v-data-table
           :headers="headers"
           :items="lecturesData"
           item-key="lecture_no"
-          class="elevation-1 !tw-mt-10 tw-border-2 tw-border-primary !tw-rounded-lg"
+          class="
+            elevation-1
+            !tw-mt-10
+            tw-border-2 tw-border-primary
+            !tw-rounded-lg
+          "
           hide-default-footer
         >
           <template v-slot:[`item.lecture_no`]="{ item }">
@@ -26,10 +35,13 @@
             <p>{{ item.subject_name }}</p>
           </template>
           <template v-slot:[`item.period`]="{ item }">
+            
+            <p class="!tw-mb-1 !tw-mt-1">
+              {{ item.day }}
+            </p>
             <p>
               {{ item.from }} -
               {{ item.to }}
-
             </p>
           </template>
         </v-data-table>
@@ -76,24 +88,25 @@ export default {
           value: 'subject_name',
           sortable: false,
         },
-        { text: 'الفترة', align: 'center', value: 'period', sortable: false },
+        { text: 'اليوم/الفترة', align: 'center', value: 'period', sortable: false },
       ],
     }
   },
   async fetch() {
     if (this.$auth.user.user_type === 'student') {
       await this.$store
-        .dispatch('students/getLecturesTable', 1)
+        .dispatch('student/getLecturesTable', this.user.id)
         .then((response) => {
           this.setAlertData(response)
         })
         .catch((error) => {
           this.setAlertData(error.response.data)
         })
-    } else if (this.$auth.user.user_type === 'lecturer') {
+    } else if (this.user.user_type === 'lecturer') {
       await this.$store
-        .dispatch('lecturers/getLecturerLectures', 1)
+        .dispatch('lecturer/getLecturerLectures', this.user.id)
         .then((response) => {
+          console.log('here ',response);
           this.setAlertData(response)
         })
         .catch((error) => {
@@ -102,8 +115,11 @@ export default {
     }
   },
   computed: {
+    user() {
+      return this.$auth.user
+    },
     lecturesData() {
-      if (this.$auth.user.user_type === 'student') {
+      if (this.user.user_type === 'student') {
         return this.$store.state.student.lectures_table_data
       } else {
         return this.$store.state.lecturer.lecturerLectures
