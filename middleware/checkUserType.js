7e7e092819742d5fca35e9e551973a9a4939aa4a
A7medRaed <ahmedraed0097@gmail.com>
2022-06-25 22:22:43 +0300

    update content add super_admin role

--- middleware/checkUserType.js
+++ middleware/checkUserType.js
@@ -2,7 +2,7 @@ export default function ({ redirect, store }) {
   if (!process.server) {
     const isAuthenticated = store.state.auth.user ? true : false
     if (isAuthenticated) {
-      if (store.state.auth.user.role === 'admin') {
+      if (store.state.auth.user.role === 'admin' || store.state.auth.user.role === 'super_admin') {
         return redirect('/admin')
       } else {
         return redirect('/')
