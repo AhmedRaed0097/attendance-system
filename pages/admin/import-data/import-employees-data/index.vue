47d1d66f83a029f8306de7474f12988da7473919
A7medRaed <ahmedraed0097@gmail.com>
2022-06-25 22:12:43 +0300

    add crud employee pages

---
+++ pages/admin/import-data/import-employees-data/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="add-member-wrapper tw-w-full">
+    <import-data-form :data-type="'employees'" />
+
+  </div>
+</template>
+
+<script>
+export default {
+   layout: 'adminLayout',
+}
+</script>
