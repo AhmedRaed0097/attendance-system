93cfe00dee329e5942cbefb55c28930e3b2e2edf
A7medRaed <83637337+AhmedRaed0097@users.noreply.github.com>
2022-06-26 20:51:50 +0300

    some changes in classes

--- pages/index.vue
+++ pages/index.vue
@@ -17,13 +17,14 @@
               d="M0,128L48,149.3C96,171,192,213,288,208C384,203,480,149,576,154.7C672,160,768,224,864,234.7C960,245,1056,203,1152,165.3C1248,128,1344,96,1392,80L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
             ></path>
           </svg>
-          <div class="avatar tw-w-20 sm:tw-w-24 md:tw-w-28">
+          <div class="avatar tw-w-20  sm:tw-w-24 md:tw-w-28">
             <img
               src="../assets/images/home/user.jpg"
               alt="user-avatar"
               class="
                 avatar
                 tw-border-2 tw-border-solid tw-border-gray-400 tw-rounded-full
+                tw-mt-1
               "
             />
           </div>
@@ -32,7 +33,7 @@
           v-if="user"
           class="user-data tw-text-center tw-mb-6 tw-mt-0 sm:tw-mt-2"
         >
-          <h3 class="tw-text-xl tw-mt-6 tw-mb-1">{{ user.name }}</h3>
+          <h3 class="tw-text-xl tw-mt-0 sm:tw-mt-4 tw-mb-1">{{ user.name }}</h3>
           <p v-if="user.user_type === 'student'" class="tw-text-xs !tw-mb-1">
             علوم الحاسوب
           </p>
