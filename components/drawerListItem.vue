ad5a1a3a3d4d3742072ee37d8e14ac6ea170ed50
A7medRaed <83637337+AhmedRaed0097@users.noreply.github.com>
2022-06-26 20:46:43 +0300

    change list item component to expansion element

--- components/drawerListItem.vue
+++ components/drawerListItem.vue
@@ -1,23 +1,15 @@
 <template>
-  <div
-    class="nav-item-link"
-    style="cursor: pointer"
-    @click="$emit('expanded')"
-  >
-    <v-list-item>
-      <v-list-item-icon>
+  <v-expansion-panel>
+    <v-expansion-panel-header disable-icon-rotate>
+      <div class="tw-flex tw-gap-4 tw-items-center">
         <slot name="icon" />
-        
-      </v-list-item-icon>
-      <v-list-item-content>
-        <v-list-item-title class="nav-item-title" active-class="active">
+        <span>
           {{ groupTitle }}
-        </v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-
-    <transition name="slide">
-      <ul v-show="isExpanded === true" class="versions-options">
+        </span>
+      </div>
+    </v-expansion-panel-header>
+    <v-expansion-panel-content class="!tw-grow">
+      <ul class="versions-options">
         <li class="nav-item-link">
           <v-list-item
             v-for="[route, text] in itemsList"
@@ -25,12 +17,12 @@
             link
             :to="route"
           >
-            <v-list-item-icon>
+            <v-list-item-icon class="!tw-flex-none">
               <img
-                width="25"
+                width="12"
                 src="~/assets/images/admin/nav-icons/list-item.svg"
                 alt="list-item"
-                class="tw-rotate-90 tw-mr-8"
+                class="tw-mr-2"
               />
             </v-list-item-icon>
 
@@ -40,8 +32,8 @@
           </v-list-item>
         </li>
       </ul>
-    </transition>
-  </div>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
 </template>
 
 <script>
@@ -55,13 +47,19 @@ export default {
       type: Array,
       default: () => [],
     },
-    isExpanded:{
-      type:Boolean,
-      default:false
-    }
+    // isExpanded: {
+    //   type: Boolean,
+    //   default: false,
+    // },
   },
 }
 </script>
 
 <style>
+.v-expansion-panel {
+  background-color: transparent !important;
+}
+.v-expansion-panel::before {
+  box-shadow: none !important;
+}
 </style>
