2b5b9144a90e5a6bd7beb2f7f817fe4ee043980a
A7medRaed <83637337+AhmedRaed0097@users.noreply.github.com>
2022-06-26 20:48:41 +0300

    some updates

--- components/navDrawer.vue
+++ components/navDrawer.vue
@@ -4,18 +4,9 @@
       app
       :right="true"
       v-model="show_drawer"
-      class="
-        !tw-z-20
-        !tw-border-primary
-        !tw-border-2
-        !tw-border-solid
-        !tw-bg-admin-primary
-      "
+      class="!tw-z-20 !tw-border-primary !tw-border-2 !tw-border-solid !tw-bg-admin-primary"
     >
-      <v-sheet
-        color="#fff"
-        class="user-data tw-py-4 !tw-bg-admin-primary-dark"
-      >
+      <v-sheet color="#fff" class="user-data tw-py-4 !tw-bg-admin-primary-dark">
         <v-avatar class="mb-4" color="#fff" size="64"></v-avatar>
 
         <div class="admin-info tw-flex">
@@ -23,25 +14,26 @@
             width="25"
             v-if="$auth.loggedIn"
             @click="$auth.logout()"
-            class="back-icon tw-ml-6 tw-cursor-pointer"
+            class="back-icon tw-ml-6 tw-mr-3 tw-cursor-pointer"
             src="~/assets/images/auth/logout.svg"
             alt="logout"
           />
-          <span class="tw-text-xl"> john@vuetifyjs.com </span>
+          <span class="tw-text-lg"
+            >{{ this.user.name }}
+            <small>
+              {{ this.user.email }}
+            </small>
+          </span>
         </div>
       </v-sheet>
 
       <v-divider></v-divider>
 
-      <v-list nav dense>
+      <!-- <v-list nav dense> -->
+      <v-expansion-panels class="!tw-pb-16" accordion>
         <!-- =============================== import Data =================================== -->
 
-        <drawerListItem
-          groupTitle="إستيراد البيانات"
-          :itemsList="import_links"
-          :isExpanded="open_import_data_options"
-          @expanded="openDropDown('import')"
-        >
+        <drawerListItem groupTitle="إستيراد البيانات" :itemsList="import_links">
           <template #icon>
             <img
               width="28"
@@ -54,12 +46,7 @@
         <!-- ================================ //import Data ================================== -->
 
         <!-- ================================ Majors ================================== -->
-        <drawerListItem
-          groupTitle="التخصصات"
-          :itemsList="major_links"
-          :isExpanded="open_majors_options"
-          @expanded="openDropDown('majors')"
-        >
+        <drawerListItem groupTitle="التخصصات" :itemsList="major_links">
           <template #icon>
             <img
               width="28"
@@ -71,12 +58,7 @@
         <!-- ================================  //Majors ================================== -->
 
         <!-- ================================  //Students ================================== -->
-        <drawerListItem
-          groupTitle="الطلاب"
-          :itemsList="student_links"
-          :isExpanded="open_students_options"
-          @expanded="openDropDown('students')"
-        >
+        <drawerListItem groupTitle="الطلاب" :itemsList="student_links">
           <template #icon>
             <img
               width="28"
@@ -90,12 +72,7 @@
 
         <!-- =============================== Lecturers =================================== -->
 
-        <drawerListItem
-          groupTitle="المحاضرين"
-          :itemsList="lecturer_links"
-          :isExpanded="open_lecturers_options"
-          @expanded="openDropDown('lecturers')"
-        >
+        <drawerListItem groupTitle="المحاضرين" :itemsList="lecturer_links">
           <template #icon>
             <img
               width="28"
@@ -109,12 +86,7 @@
 
         <!-- =============================== Subjects =================================== -->
 
-        <drawerListItem
-          groupTitle="المواد"
-          :itemsList="subject_links"
-          :isExpanded="open_subjects_options"
-          @expanded="openDropDown('subjects')"
-        >
+        <drawerListItem groupTitle="المواد" :itemsList="subject_links">
           <template #icon>
             <img
               width="28"
@@ -127,12 +99,7 @@
         <!-- ================================ //Subjects ================================== -->
 
         <!-- ================================ Periods ================================== -->
-        <drawerListItem
-          groupTitle="الفترات"
-          :itemsList="period_links"
-          :isExpanded="open_periods_options"
-          @expanded="openDropDown('periods')"
-        >
+        <drawerListItem groupTitle="الفترات" :itemsList="period_links">
           <template #icon>
             <img
               width="28"
@@ -146,12 +113,7 @@
 
         <!-- ================================ Lectures ================================== -->
 
-        <drawerListItem
-          groupTitle="المحاضرات"
-          :itemsList="lecture_links"
-          :isExpanded="open_lectures_options"
-          @expanded="openDropDown('lectures')"
-        >
+        <drawerListItem groupTitle="المحاضرات" :itemsList="lecture_links">
           <template #icon>
             <img
               width="28"
@@ -163,12 +125,7 @@
         <!-- ================================= //Lectures ================================ -->
 
         <!-- =============================== Tables =================================== -->
-        <drawerListItem
-          groupTitle="الجداول"
-          :itemsList="table_links"
-          :isExpanded="open_tables_options"
-          @expanded="openDropDown('tables')"
-        >
+        <drawerListItem groupTitle="الجداول" :itemsList="table_links">
           <template #icon>
             <img
               width="28"
@@ -180,13 +137,11 @@
 
         <!-- ================================ //Tables ================================== -->
 
-        <!-- =============================== employees =================================== -->
+        <!-- =============================== Employees =================================== -->
         <drawerListItem
-        v-if="this.user.role === 'super_admin'"
+          v-if="this.user.role === 'super_admin'"
           groupTitle="الموظفين"
           :itemsList="employee_links"
-          :isExpanded="open_employees_options"
-          @expanded="openDropDown('employees')"
         >
           <template #icon>
             <img
@@ -197,8 +152,24 @@
           </template>
         </drawerListItem>
 
-        <!-- ================================ //employees ================================== -->
-      </v-list>
+        <!-- ================================ //Employees ================================== -->
+
+        <!-- =============================== Reports =================================== -->
+
+        <div class="tw-w-[82%] tw-flex tw-items-center tw-gap-4">
+          <img
+            width="30"
+            src="../assets/images/admin/nav-icons/employees.svg"
+            alt="icon"
+          />
+          <nuxt-link to="/admin/reports" class="tw-cursor-pointer">
+            التقارير
+          </nuxt-link>
+        </div>
+
+        <!-- ================================ //Reports ================================== -->
+      </v-expansion-panels>
+      <!-- </v-list> -->
     </v-navigation-drawer>
 
     <v-app-bar color="#2a3588" height="70" app class="app-bar">
